4262eabd87ebac3ea3c1900a38f2df60adcd968b
sujathachen-123 <sujathagovardhan1703@gmail.com>
2024-11-28 16:45:31 +0530

    commit-1

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+// import App from './App';
+import reportWebVitals from './reportWebVitals';
+// import Tables from './Tables.js';
+import Cards from './practice.js/practice1.js'
+// import ConditionalRend from './practice.js/nov28.js'
+// import MainComp from './practice.js/parent.js';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    {/* <Tables/> */}
+    // <Cards/>
+    {/* <ConditionalRend isLoggedIn={false}/> */}
+    {/* <MainComp login="admin"/> */}
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
